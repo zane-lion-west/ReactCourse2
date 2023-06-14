1d6574168d1a51fc45495c5bac159cdf905e7325
Charlie Jacques <charlie.jxcques@gmail.com>
2023-06-14 12:58:45 +0100

    pushing everything i have

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,42 @@
 import JokeComponent from './JokeComponent/JokeComponent';
 
 export { JokeComponent };
+
+/*
+
+  JavaScript Functions:
+
+  function myFunction() {
+
+  }
+
+  const myFunction = () => {
+    run any code here
+    mostly used for running multiple lines of code
+    return any value if you like
+  }
+
+  () => (run a single statement and the value of that statement will be returned)
+
+  statement = any thing you can run in 1 line
+    () => myFunction() // The return value of myFunction() will be returned
+    () => showElement // will return the variable showElement
+    () => (showElement ? <Element /> : undefined) // will return <Element /> if showElement is `true` else will return undefined
+
+*/
+
+/*
+
+  Javascript Comments:
+
+  Single Line: 
+  // This is a single line comment
+
+  Multiple Line: 
+  /* and then end with the thing at the bottom
+
+  JSDoc Comment:
+  /** then end with the thing at the bottom (only one star on the closing tag)
+  
+  
+*/
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from 'react';
 import s from './Home.module.scss';
+
+/**
+ * @typedef Joke
+ * @property {string} punchline - This is the punchline of the joke
+ * @property {string} setup
+ * @property {number} id
+ * @property {string} type
+ *
+ * Returns 10 jokes from the github api {@link https://github.com/15Dkatz/official_joke_api}
+ * @param {boolean} [programming=false]
+ * @returns {Promise<Joke[]>}
+ */
+const getJokes = (programming = false) =>
+  fetch(
+    `https://official-joke-api.appspot.com/jokes/${
+      programming ? 'programming/' : ''
+    }ten`,
+  ).then(data => data.json());
 
 /**
  * Home Page
  * @returns {JSX.Element}
  */
-
 const Home = () => {
-  // Get Jokes and render them in a JokeComponent
+  const [jokes, setJokes] = useState();
+
+  useEffect(() => {
+    if (!jokes) {
+      getJokes()
+        .then(jokes => setJokes(jokes))
+        .catch(e => console.error(e.message));
+    }
+  }, [jokes]);
+
   return <div className={s.wrapper}>Home</div>;
 };
 
